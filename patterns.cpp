641d443fdbe381718d21979e0882c7fcd0d2428e
Rrahullkumar <rahulkr811844@gmail.com>
2025-06-24 22:24:18 +0530

    more patterns

--- patterns.cpp
+++ patterns.cpp
@@ -102,8 +102,9 @@ int main()
     1234
     123
     12
+    1
     */
-    // for (int i = 5; i > 0; i--)
+    // for (int i = 5; i >= 0; i--)
     // {
     //     for (int j = 1; j <= i; j++)
     //     {
@@ -118,20 +119,42 @@ int main()
      ***
     *****
     */
-    int n = 3;
 
-    for (int row = 1; row <= n; row++)
+    // int n = 3;
+
+    // for (int row = 1; row <= n; row++)
+    // {
+    //     for (int space = 1; space <= n-row; space++)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int star = 1; star <= 2*row-1; star++)
+    //     {
+    //         cout<<"*";
+    //     }       
+    //     cout<<endl;
+    // }
+
+    /*
+    *******
+     *****
+      ***
+       *
+    */
+
+    int n=5;
+    for (int row= 1; row <= n; row++)
     {
-        for (int space = 1; space <= n-row; space++)
+        for (int space = 1; space < row; space++)
         {
             cout<<" ";
-        }
-        for (int star = 1; star <= 2*row-1; star++)
+        }        
+        for (int col = 9; col >=2*row+1 ; col--)
         {
             cout<<"*";
         }
-        
         cout<<endl;
+        
     }
 
     
