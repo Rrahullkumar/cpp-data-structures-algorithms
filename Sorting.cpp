d98fa5003274b45bdf48a69e0c33975626672de6
Rrahullkumar <rahulkr811844@gmail.com>
2025-07-12 20:05:00 +0530

    Bubble sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // Selection sort
+/*
 int main(){
 
     int arr[6] = {10,50,20,30,15,25};
@@ -30,5 +31,35 @@ int main(){
         cout<<arr[i]<<endl;
     }
     
+}
+*/
+
+// Bubble sort : Push the maximum to the last by adjecent swaps
+
+int main(){
+
+    int arr[6] = {10,50,20,30,15,25};
+
+    int n= 6;
+
+    for (int i = n-1; i >=1; i--)
+    {
+        for (int j = 0; j <= i-1 ; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] =arr[j+1];
+                arr[j+1] = temp;
+            }
+            
+        }
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
     
 }
